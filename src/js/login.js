ef6303fed8b6c7eece09c70c545756e1acbc910c
Zac Waite <129032699zw@gmail.com>
2022-02-24 12:41:12 -0500

    Login and registraton integrated

---
+++ src/js/login.js
@@ -0,0 +1,21 @@
+const tryLogin = async () => {
+    let usernameInput = document.getElementById("username");
+    let passwordInput = document.getElementById("pword");
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    if (username===""||password==="") return;
+    let json = await httpReq("/auth/signin.php/", "POST", {
+        password: password,
+        username: username,
+    })
+    let response = JSON.parse(json);
+    if (response.success && response.objects) {
+        alert("Success")
+        redirect("/home.html");
+        setCookie("username", username);
+        setCookie("token", response.objects.token);
+    } else if (response.errors.length > 0) {
+        alert("Failure");
+        alert(JSON.stringify(response.errors))
+    }
+}
---
+++ src/js/register.js
@@ -0,0 +1,31 @@
+const tryRegister = async () => {
+    let usernameInput = document.getElementById("username");
+    let passwordInput = document.getElementById("pword");
+    let password2Input = document.getElementById("pword2");
+    let emailInput = document.getElementById("email");
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    let password2 = password2Input.value;
+    let email = emailInput.value;
+    if (username===""||password===""||email==="") return;
+    if (password!==password2) {
+        alert("passwords don't match");
+        return;
+    }
+    let json = await httpReq("/api/user/", "POST", {
+        email: email,
+        password: password,
+        username: username,
+        admin: true
+    })
+    let response = JSON.parse(json);
+    if (response.success && response.objects) {
+        alert("Success")
+        redirect("/home.html");
+        setCookie("username", username);
+        setCookie("token", response.objects.token);
+    } else if (response.errors.length > 0) {
+        alert("Failure");
+        alert(JSON.stringify(response.errors))
+    }
+}
